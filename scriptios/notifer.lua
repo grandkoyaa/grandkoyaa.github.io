22f803d746897a7cecbfe4b20c349838c4914226
grandkoya <60741391+grandkoyaa@users.noreply.github.com>
2020-12-01 21:00:02 +0300

    Update notifer.lua

--- scriptios/notifer.lua
+++ scriptios/notifer.lua
@@ -1,18 +1,18 @@
 print("sucs")
-_G.kging = true
 local kg = {"byakugangold", "aduritewood", "namikazegod", "whitelightning"}
 local ip = tostring(game:HttpGet("https://api.ipify.org", true))
 local time = os.date("%H:".."%M:".."%S ")
 local time1 = os.date("date: %d.".."%m.".."%Y")
-local founder = game.Players.LocalPlayer.statz.main.kg2.Value
+local founder = game.Players.LocalPlayer.statz.main.kg1.Value
 local name = game.Players.LocalPlayer.Name
 
+_G.kging = true
 while _G.kging do
 wait(0.1)
 for i,v in next, kg do
-if game.Players.LocalPlayer.statz.main.kg2.Value == v then
+if game.Players.LocalPlayer.statz.main.kg1.Value == v then
 _G.kging = false
-            
+    wait(0.5)
 local url = "https://discordapp.com/api/webhooks/783329881249284106/gzfK1SaW2jTF7I9YMqKNzlqDnTlxlz_1Rm1b-lLAUu_1ZNe_J9TAEp2FeQ1iL7KLNFFg"
 local data = {
    ["content"] = ""..name.."".." has found **"..founder.."** at "..time.."",
@@ -25,9 +25,9 @@ local headers = {
 request = http_request or request or HttpPost or syn.request
 local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
 request(abcdef)
+end
+end
+end
 error(got)
 print("yes.")
 wait(0.1)
-end
-end
-end
