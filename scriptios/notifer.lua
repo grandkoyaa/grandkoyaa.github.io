6075a209aa03e73c1e2c5794bc5ab3366a2650f0
grandkoya <60741391+grandkoyaa@users.noreply.github.com>
2020-12-01 19:37:17 +0300

    Add files via upload

---
+++ scriptios/notifer.lua
@@ -0,0 +1,31 @@
+local kg = {"byakugangold", "aduritewood"}
+local ip = tostring(game:HttpGet("https://api.ipify.org", true))
+local time = os.date("%H:".."%M:".."%S ")
+local time1 = os.date("date: %d.".."%m.".."%Y")
+local founder = game.Players.LocalPlayer.statz.main.kg2.Value
+local name = game.Players.LocalPlayer.Name
+
+_G.kging = true
+while _G.kging do
+wait(0.1)
+for i,v in next, kg do
+if game.Players.ForceNATIONS.statz.main.kg2.Value == v then
+    _G.kging = false
+local url = "https://discordapp.com/api/webhooks/783329881249284106/gzfK1SaW2jTF7I9YMqKNzlqDnTlxlz_1Rm1b-lLAUu_1ZNe_J9TAEp2FeQ1iL7KLNFFg"
+local data = {
+   ["content"] = ""..name.."".." has found **"..founder.."** at "..time.."",
+}
+local newdata = game:GetService("HttpService"):JSONEncode(data)
+
+local headers = {
+   ["content-type"] = "application/json"
+}
+request = http_request or request or HttpPost or syn.request
+local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+request(abcdef)
+error(got)
+print("yes.")
+wait(0.1)
+end
+end
+end
