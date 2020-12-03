5e7ef32b73d790e253008d187cc3116fd7fbefd8
grandkoya <60741391+grandkoyaa@users.noreply.github.com>
2020-12-03 23:20:26 +0300

    Update bald.lua

--- scriptios/bald.lua
+++ scriptios/bald.lua
@@ -1,6 +1,9 @@
+while true do
+  wait(.1)
 game.Workspace.H4RDBOIII["Left Arm"].CanCollide = false
 game.Workspace.H4RDBOIII["Right Arm"].CanCollide = false
 game.Workspace.H4RDBOIII["Left Leg"].CanCollide = false
 game.Workspace.H4RDBOIII["Right Leg"].CanCollide = false
 game.Workspace.H4RDBOIII["Torso"].CanCollide = false
 game.Workspace.H4RDBOIII["Head"].CanCollide = false
+end
