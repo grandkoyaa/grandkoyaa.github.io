c1328d87ad1315742a47fa401a4cf457c7ca28d4
grandkoya <60741391+grandkoyaa@users.noreply.github.com>
2020-12-03 23:16:47 +0300

    Create bald.lua

---
+++ scriptios/bald.lua
@@ -0,0 +1,6 @@
+game.Workspace.H4RDBOIII["Left Arm"].CanCollide = false
+game.Workspace.H4RDBOIII["Right Arm"].CanCollide = false
+game.Workspace.H4RDBOIII["Left Leg"].CanCollide = false
+game.Workspace.H4RDBOIII["Right Leg"].CanCollide = false
+game.Workspace.H4RDBOIII["Torso"].CanCollide = false
+game.Workspace.H4RDBOIII["Head"].CanCollide = false
